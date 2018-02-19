39fd9b2a49236e85df25ee2cf504aa39c283372f
Michael Tambornino <michael.tambornino@gmail.com>
2018-02-19 16:05:17 -0600

    Initial commit

---
+++ src/com/pluralsight/calcengine/Main.java
@@ -0,0 +1,43 @@
+package com.pluralsight.calcengine;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        MathEquation test = new MathEquation();
+//        test.execute();
+//        System.out.print("Test: ");
+//        System.out.println(test.getResult());
+
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = new MathEquation('d',100.0d, 50.d);
+        equations[1] = new MathEquation('a',25.0d, 92.d);
+        equations[2] = new MathEquation('s',225.0d, 17.d);
+        equations[3] = new MathEquation('m',11.0d, 3.0d);
+
+        for (MathEquation equation : equations) { // for each EQUATION in EQUATIONS
+            equation.execute();
+            System.out.print("Result: ");
+            System.out.println(equation.getResult());
+        }
+
+        System.out.println();
+        System.out.println("Using Overloads");
+        System.out.println();
+
+        double leftDouble = 9.0d;
+        double rightDouble = 4.0d;
+        int leftInt = 9;
+        int rightInt = 4;
+
+        MathEquation equationOverload = new MathEquation('d');
+
+        equationOverload.execute(leftDouble, rightDouble);
+        System.out.print("Result: ");
+        System.out.println(equationOverload.getResult());
+
+        equationOverload.execute(leftInt, rightInt);
+        System.out.print("Result: ");
+        System.out.println(equationOverload.getResult());
+
+    }
+}
